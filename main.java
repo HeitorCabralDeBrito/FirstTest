d3fb15cd21fb69b1596bdf6dea5104481519be67
Heitor Cabral de Brito <heitorcbrito@gmail.com>
2026-01-04 20:32:33 -0300

    Adding code

--- main.java
+++ main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args){
 
-        System.out.println("Hello, world");
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Write the first number: ");
+        int number1 = keyboard.nextInt();
+
+        System.out.print("Write the second number: ");
+        int number2 = keyboard.nextInt();
+
+        int sum = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " + " + number2 + " is " + sum );
 
     }
 
